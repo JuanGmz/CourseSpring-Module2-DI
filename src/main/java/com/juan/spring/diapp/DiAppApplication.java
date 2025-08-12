b529d1f3ca9bfa36d786939cfe9910712644d0c9
JuanGmz <juanalfredohd@gmail.com>
2025-08-12 15:27:57 -0600

    first commit

--- src/main/java/com/juan/spring/diapp/DiAppApplication.java
+++ src/main/java/com/juan/spring/diapp/DiAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiAppApplication.class, args);
+    }
 
 }
